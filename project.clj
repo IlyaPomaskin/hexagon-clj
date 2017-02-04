387a9249ebc0b6dcfc425dc10959c46343a58ba4
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2017-02-04 21:03:56 +0300

    chore(project): version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hexagon "0.1.0-SNAPSHOT"
+(defproject hexagon "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
