eb1d4be55fd553bab58671d15ed62aa74e899892
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2017-02-04 18:37:47 +0300

    feat(log): log for game

--- src/hexagon/log.clj
+++ src/hexagon/log.clj
@@ -15,3 +15,8 @@
 (def user-error (log "user" "ERR"))
 (def user-debug (log "user" "DEBUG"))
 (def user-debug (fn [& args] nil))
+(def game-info (log "game" "INFO"))
+(def game-error (log "game" "ERR"))
+(def game-debug (log "game" "DEBUG"))
+(def game-debug (fn [& args] nil))
+
