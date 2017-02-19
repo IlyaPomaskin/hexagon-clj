80c95a82b24aa80185462dda36c14ef6826052b6
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2017-02-19 20:15:07 +0300

    feat(board): add "test" board

--- src/hexagon/entities/board.clj
+++ src/hexagon/entities/board.clj
@@ -18,12 +18,38 @@
                                    :cell/owner :red }
                                  { :cell/x 1
                                    :cell/y 1
-                                   :cell/type nil }]}
+                                   :cell/type nil }] }
                    { :board/name "modern"
                      :board/map [{ :cell/x 2
                                    :cell/y 1
                                    :cell/type :normal
-                                   :cell/owner nil }]}]))
+                                   :cell/owner nil }] }
+                   { :board/name "test"
+                     :board/map [{:cell/x 0, :cell/y 0, :cell/type :normal, :cell/owner :red}
+                                 {:cell/x 0, :cell/y 1, :cell/type :normal}
+                                 {:cell/x 0, :cell/y 2, :cell/type :normal}
+                                 {:cell/x 0, :cell/y 3, :cell/type :normal}
+                                 {:cell/x 0, :cell/y 4, :cell/type :normal}
+                                 {:cell/x 1, :cell/y 0, :cell/type :normal}
+                                 {:cell/x 1, :cell/y 1, :cell/type :normal}
+                                 {:cell/x 1, :cell/y 2, :cell/type :normal}
+                                 {:cell/x 1, :cell/y 3, :cell/type :normal}
+                                 {:cell/x 1, :cell/y 4, :cell/type :normal}
+                                 {:cell/x 2, :cell/y 0, :cell/type :normal}
+                                 {:cell/x 2, :cell/y 1, :cell/type :normal}
+                                 {:cell/x 2, :cell/y 2, :cell/type :normal}
+                                 {:cell/x 2, :cell/y 3, :cell/type :normal}
+                                 {:cell/x 2, :cell/y 4, :cell/type :normal}
+                                 {:cell/x 3, :cell/y 0, :cell/type :normal}
+                                 {:cell/x 3, :cell/y 1, :cell/type :normal}
+                                 {:cell/x 3, :cell/y 2, :cell/type :normal}
+                                 {:cell/x 3, :cell/y 3, :cell/type :normal}
+                                 {:cell/x 3, :cell/y 4, :cell/type :normal}
+                                 {:cell/x 4, :cell/y 0, :cell/type :normal}
+                                 {:cell/x 4, :cell/y 1, :cell/type :normal}
+                                 {:cell/x 4, :cell/y 2, :cell/type :normal}
+                                 {:cell/x 4, :cell/y 3, :cell/type :normal}
+                                 {:cell/x 4, :cell/y 4, :cell/type :normal, :cell/owner :blue}] }]))
 
 (def default
   (db/eid-by-av :board/name "classic"))
