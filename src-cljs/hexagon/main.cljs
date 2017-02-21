e0ecab3d276c88513e62d10bb6318a16fdb0fede
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2017-02-22 01:26:39 +0300

    feat(cljs/main): receive datoms

--- src-cljs/hexagon/main.cljs
+++ src-cljs/hexagon/main.cljs
@@ -1,9 +1,17 @@
 (ns hexagon.main
   (:require [rum.core :as rum]
             [datascript.core :as d]
-            [hexagon.db :as db :refer [db]]))
+            [hexagon.db :as db :refer [db]]
+            [hexagon.ws :as ws]))
 
 (enable-console-print!)
+
+(defn handle-message [msg]
+  (when (= (.-type msg) "datoms")
+    (->> msg
+       .-payload
+       cljs.reader/read-string
+       (d/transact! db))))
 
 (rum/defcs stateful < (rum/local 0 ::key)
   [state label]
@@ -12,6 +20,11 @@
      label ": " @local-atom]))
 
 (defn init! []
+  (when-some [ws @ws/chan]
+             (do
+               (.close ws)
+               (db/reset!)))
+  (ws/make! "username2" handle-message)
   (rum/mount (stateful "Click count") (. js/document (querySelector "#container"))))
 
 (init!)
