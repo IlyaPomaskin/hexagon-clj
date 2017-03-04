7a317c8632521ad73442f9a3a23788ab93c2ba30
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2017-03-04 12:51:47 +0300

    feat(views): initial state

--- src/hexagon/views.clj
+++ src/hexagon/views.clj
@@ -1,5 +1,6 @@
 (ns hexagon.views
   (:require
+    [cheshire.core :as json]
     [hiccup
      [page :refer [html5]]
      [page :refer [include-js]]]))
@@ -16,10 +17,11 @@
    [:meta { :name "viewport"
             :content "width=device-width, initial-scale=1" }]])
 
-(defn body [additional]
+(defn body [& additional]
   [:body.c-text
-   [:h1.c-heading.u-centered "Hexagon"]
-   additional])
+   (concat
+     [[:h1.c-heading.u-centered "Hexagon"]]
+     additional)])
 
 (defn index-page
   ([]
@@ -44,9 +46,11 @@
            [:input.c-button { :type "submit"
                               :value "Login" }]]]]]))))
 
-(defn game-page []
+(defn game-page [initial-state]
   (html5
     head
     (body
-      [[:div#container]
-       (include-js "/js/main.js")])))
+      [:div#container]
+      [:script { :type "text/javascript" }
+       (str "window.__INITIAL_STATE__ = " (str (json/encode initial-state)) ";")]
+      (include-js "/js/main.js"))))
