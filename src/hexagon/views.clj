ae0d1ff9ffed3ef845985a460d71a6debef0bdd6
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2017-03-04 12:35:44 +0300

    feat(views): common body

--- src/hexagon/views.clj
+++ src/hexagon/views.clj
@@ -13,34 +13,37 @@
       [:meta {:name "viewport"
               :content "width=device-width, initial-scale=1"}]])
 
+(defn body [additional]
+  [:body.c-text
+   [:h1.c-heading.u-centered "Hexagon"]
+   additional])
+
 (defn index-page
   ([]
    (index-page {}))
   ([{ username-error :username }]
    (html5
      head
-     [:body.c-text
-      [:h1.c-heading.u-centered "Hexagon"]
-      [:div.o-container.o-container--xsmall
-       [:form { :method "post"
-                :action "/" }
-        [:fieldset.o-fieldset
-         [:h3.c-heading.u-centered "Enter your username"]
-         [:div.o-form-element
-          [:input { :type "text"
-                    :name "username"
-                    :class (str "c-field"
-                                (when username-error " c-field--error"))}]
-          (when username-error
-            [:div.c-hint.c-hint--static.c-hint--error username-error])]
-         [:div.o-form-element.u-centered
-          [:input.c-button { :type "submit"
-                             :value "Login" }]]]]]])))
+     (body
+       [:div.o-container.o-container--xsmall
+        [:form { :method "post"
+                 :action "/" }
+         [:fieldset.o-fieldset
+          [:h3.c-heading.u-centered "Enter your username"]
+          [:div.o-form-element
+           [:input { :type "text"
+                     :name "username"
+                     :class (str "c-field"
+                                 (when username-error " c-field--error"))}]
+           (when username-error
+             [:div.c-hint.c-hint--static.c-hint--error username-error])]
+          [:div.o-form-element.u-centered
+           [:input.c-button { :type "submit"
+                              :value "Login" }]]]]]))))
 
 (defn game-page []
   (html5
     head
-    [:body.c-text
-     [:h1 "Hexagon"]
-     [:div#container]
-     (include-js "/js/main.js")]))
+    (body
+      [[:div#container]
+       (include-js "/js/main.js")])))
