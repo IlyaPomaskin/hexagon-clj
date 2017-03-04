d8c73648dcc7abb02101707e3a5337340906944f
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2017-03-04 12:21:30 +0300

    feat(routes): validate username

--- src/hexagon/routes.clj
+++ src/hexagon/routes.clj
@@ -1,14 +1,30 @@
 (ns hexagon.routes
   (:use compojure.core
-        hexagon.views
         [hiccup.middleware :only (wrap-base-url)]
         [org.httpkit.server :only (run-server)])
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
+            [clojure.string :as string]
+            [hexagon.entities.user :as user]
+            [hexagon.views :as view]
             [hexagon.ws :as ws]))
 
+(defn validate-form [form]
+  (let [username (get form "username")]
+    (cond
+      (string/blank? username) { :username "Enter username" }
+      (user/exists? username) { :username "Username already used" }
+      :else nil)))
+
+(defn authorize [{ form :form-params }]
+  (if-let [errors (validate-form form)]
+    (view/index-page errors)
+    (println "OK create sessions" form)))
+
 (defroutes main-routes
-  (GET "/" [] (index-page))
+  (context "/" []
+           (GET "/" [] (view/index-page))
+           (POST "/" [] authorize))
   (GET "/ws" request (ws/ws-handler request))
   (route/resources "/")
   (route/not-found "Page not found"))
